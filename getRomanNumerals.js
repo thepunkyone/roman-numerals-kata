ddf1f1501fcc262c9af5483363a2d68e03db1db1
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-22 10:44:34 +0100

    refactored into additional functions

--- getRomanNumerals.js
+++ getRomanNumerals.js
@@ -1,34 +1,52 @@
+printSingleDigits = (number) => {
+    if (number < 4) {
+        return 'I'.repeat(number)
+    }
+
+    if (number === 4) return 'IV'
+
+    if (number === 5) return 'V'
+
+    if (number === 9) return 'IX'
+
+    if (number > 5 && number < 9) {
+        return 'V' + 'I'.repeat(number - 5)
+    }
+
+    return ''
+}
+
+printDoubleDigits = (number) => {
+    if (number === 40) return 'XL'
+
+    if (number === 50) return 'L'
+
+    if (number === 90) return 'XC'
+
+    if ((number === 10 || number % 10 === 0) && number < 50) {
+        return 'X'.repeat(number / 10)
+    }
+
+    if (number % 10 === 0 && number > 50) {
+        return 'L' + 'X'.repeat((number - 50) / 10)
+    }
+
+    return ''
+}
+
 getRomanNumerals = (number) => {
     if (number) {
-        if (number < 4) {
-            return 'I'.repeat(number)
+        const numbersArray = number.toString().split('').map(item => Number(item))
+
+        if (numbersArray.length === 1) {
+            return printSingleDigits(number)
         }
 
-        if (number === 4) return 'IV'
-
-        if (number === 5) return 'V'
-
-        if (number === 9) return 'IX'
-
-        if (number === 40) return 'XL'
-
-        if (number === 50) return 'L'
-
-        if (number === 90) return 'XC'
+        if (numbersArray.length === 2) {
+            return printDoubleDigits(number);
+        }   
 
         if (number === 100) return 'C'
-
-        if (number > 5 && number < 9) {
-            return 'V' + 'I'.repeat(number - 5)
-        }
-
-        if ((number === 10 || number % 10 === 0) && number < 50) {
-            return 'X'.repeat(number / 10)
-        }
-
-        if (number % 10 === 0 && number > 50) {
-            return 'L' + 'X'.repeat((number - 50) / 10)
-        }
 
     }
     return ''
